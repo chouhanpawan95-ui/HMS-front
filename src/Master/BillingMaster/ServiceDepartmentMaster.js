7ba1a5e788f44ee502c5e4cb01285993e2f606a9
priyachouhan11 <priyachouhan6910@gmail.com>
2026-02-03 16:22:22 +0530

    servicedepartmnetmaster responsive

--- src/Master/BillingMaster/ServiceDepartmentMaster.js
+++ src/Master/BillingMaster/ServiceDepartmentMaster.js
@@ -1,73 +1,105 @@
-import { Button, Container, TextField, Typography } from "@mui/material";
+import { Button, Container, TextField, Typography, Box, Paper, Grid } from "@mui/material";
 import styles from "../../component/Container.module.css";
-import {useGetServiceDepartmentMasterQuery,useCreateServiceDepartmentMasterMutation} from '../../features/api/Hooks/serviceApi.js';
+import { useGetServiceDepartmentMasterQuery, useCreateServiceDepartmentMasterMutation } from '../../features/api/Hooks/serviceApi.js';
 import Loader from "../../component/Loader";
 import { useForm } from "react-hook-form";
+import style from "../BillingMaster/RateListMaster.module.css";
 
-
-const ServiceDepartmentMaster =() =>{
-  const{data:serviceDepartmentMaster,isLoading,refetch} = useGetServiceDepartmentMasterQuery();
+const ServiceDepartmentMaster = () => {
+  const { data: serviceDepartmentMaster, isLoading, refetch } = useGetServiceDepartmentMasterQuery();
   console.log("Service Department Master Data:", serviceDepartmentMaster);
-  const[createServiceDepartmentMaster] = useCreateServiceDepartmentMasterMutation();
+  const [createServiceDepartmentMaster] = useCreateServiceDepartmentMasterMutation();
   console.log("Create Service Department Master Mutation:", createServiceDepartmentMaster);
 
-  const{
+  const {
     register,
     handleSubmit,
     reset,
-    formState:{errors},
+    formState: { errors },
   } = useForm();
 
-  const onSubmit = async(formData) => {
-    try{
+  const onSubmit = async (formData) => {
+    try {
       await createServiceDepartmentMaster({
-        DeptName:formData.name,
-        DeptType:formData.type,
-        SeqNo:formData.seqNo
+        DeptName: formData.name,
+        DeptType: formData.type,
+        SeqNo: formData.seqNo
       }).unwrap();
       reset();
       refetch();
-    }catch(err){
+    } catch (err) {
       console.error("Failed to add department: ", err);
       console.log('Api validation error message:', err.data);
-      alert("backend error"+JSON.stringify(err.data));
+      alert("backend error" + JSON.stringify(err.data));
     }
   };
 
-  if(isLoading) return <Loader></Loader>
+  if (isLoading) return <Loader></Loader>
 
-  return(
-    <Container className={styles.container}>
-      
-      <Typography variant="h4" className={styles.header} sx={{mb:2}}>Service Deptartment Master</Typography>
-
-      <form 
-      onSubmit={handleSubmit(onSubmit)}
-      className={styles.form}>
-        <TextField className={styles.text}
-        label='Department Name'
-        {...register ('name',{required:"Department Name is required"})}
-        error={!!errors.name}
-        helperText={errors.name?.message}
-        />
-        <TextField className={styles.text}
-        label='Department Type'
-        {...register ('type',{required:"Department Type is required"})}
-        error={!!errors.type}
-        helperText={errors.type?.message}
-        />
-        <TextField className={styles.text}
-        label='Department SeqNo'
-        {...register ('seqNo',{required:"Department SeqNo is required"})}
-        error={!!errors.seqNo}
-        helperText={errors.seqNo?.message}
-        />
-
-        <Button className={styles.button} type="submit" variant="contained">Save</Button>
-
-      </form>
-
-    </Container>
+  return (
+    <Box sx={{
+      p: { xs: 2, sm: 3, md: 4 },
+      backgroundColor: "#f4f6f8",
+      minHeight: "100vh",
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-start'
+    }}>
+      <Paper elevation={4}
+        sx={{
+          maxWidth: { xs: "95%", sm: "90%", md: 960 },
+          width: '100%',
+          mb: 4,
+          // mx: "auto",
+          borderRadius: 3,
+          mt: { xs: 2, sm: 4, md: 6 },
+          overflow: "hidden",
+          background: "White",
+          p: { xs: 2, sm: 3, md: 4 },
+        }}>
+        <Box>
+          <Typography variant="h4" className={style.header} >Service Deptartment Master</Typography>
+        </Box>
+        <Box sx={{ p: { xs: 2, sm: 3, md: 4 } }}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2} style={{display: 'flex',flexDirection: 'column'}}>
+              <Grid item xs={12} md={6}>
+                <TextField 
+                  fullWidth
+                  size="small"
+                  label='Department Name'
+                  {...register('name', { required: "Department Name is required" })}
+                  error={!!errors.name}
+                  helperText={errors.name?.message}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField  
+                  fullWidth
+                  size="small"
+                  label='Department Type'
+                  {...register('type', { required: "Department Type is required" })}
+                  error={!!errors.type}
+                  helperText={errors.type?.message}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField  
+                  fullWidth
+                  size="small"
+                  label='Department SeqNo'
+                  {...register('seqNo', { required: "Department SeqNo is required" })}
+                  error={!!errors.seqNo}
+                  helperText={errors.seqNo?.message}
+                />
+              </Grid>
+              <Button className={styles.button} type="submit" variant="contained">Save</Button>
+            </Grid>
+          </form>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
 export default ServiceDepartmentMaster;
