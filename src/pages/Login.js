b558b56f293e7bce0fb0d5106bf40bab5413c42c
chouhanpawan95-ui <chouhanpawan95@gmail.com>
2026-02-05 14:39:14 +0530

    manage route

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -49,7 +49,7 @@ function Login() {
 
       if (data?.token) {
         localStorage.setItem('token', data.token);
-        navigate('/');
+        navigate('/Dashboard');
       } else {
         throw new Error('No token received from server');
       }
